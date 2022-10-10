0e9a2d2b37ccb2de59e70bff4c712869ceed95ca
Chris Fanucchi <admin@knightskeep.com>
2022-10-10 15:12:22 -0700

    Setup basic REST-CRUD methods for user-DB

---
+++ src/models/users.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  zipCode: { type: String, required: true },
+  favFoods: { type: Array, required: false },
+  favActivities: { type: Array, required: false },
+  priorSearches: { type: Array, required: false },
+  savedActivities: { type: Array, required: false },
+});
+
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
