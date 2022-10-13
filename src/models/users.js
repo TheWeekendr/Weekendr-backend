e4895c042779b380425043593f25d4992e7c6c38
Chris Fanucchi <admin@knightskeep.com>
2022-10-13 09:39:51 -0700

    Changed delete of user data to delete by userSub

--- src/models/users.js
+++ src/models/users.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
+  userSub: { type: String, required: true },
   name: { type: String, required: true },
   zipCode: { type: String, required: true },
   favFoods: { type: Array, required: false },
